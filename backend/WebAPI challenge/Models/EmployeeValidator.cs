359d9ef2f2dfe05066bd11c0aaec193478d81271
luquinhasCS <lucasdecarvalho669@gmail.com>
2024-02-25 23:47:07 -0300

    Implemented a basic validation when inputing the employee data on creation.

---
+++ backend/WebAPI challenge/Models/EmployeeValidator.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+
+namespace WebAPI_challenge.Models
+{
+    public class EmployeeValidator : AbstractValidator<EmployeeModel>
+
+    {
+        public EmployeeValidator() {
+            RuleFor(p => p.Name)
+                            .NotEmpty()
+                            .WithMessage("Name is required");
+
+            RuleFor(p => p.Surname)
+                            .NotEmpty()
+                            .WithMessage("Surname is required");
+
+            RuleFor(p => p.Shift)
+                .IsInEnum()
+                .WithMessage("Shift is required");
+
+            RuleFor(p => p.Department)
+                .IsInEnum()
+                .WithMessage("Department is required");
+
+        }
+    }
+}
--- backend/WebAPI challenge/Program.cs
+++ backend/WebAPI challenge/Program.cs
@@ -1,3 +1,4 @@
+using FluentValidation.AspNetCore;
 using Microsoft.EntityFrameworkCore;
 using WebAPI_challenge.DataContext;
 using WebAPI_challenge.Service.EmployeeService;
@@ -6,7 +7,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddFluentValidation(config =>
+{
+    config.RegisterValidatorsFromAssembly(typeof(Program).Assembly);
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
